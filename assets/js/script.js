3f421d651fe06d523b96a52fc30be15968b40d52
FelipeCarquio <148922884+FelipeCarquio@users.noreply.github.com>
2025-09-20 21:36:51 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
+let icone = document.getElementById('icone')
+
 function clickMenu() {
     let menu = document.getElementById('menu')    
     if (menu.style.display == 'block') {
         menu.style.display = 'none'
+        icone.classList.replace('bi-x-lg', 'bi-list')
     }else {
         menu.style.display = 'block'
+        icone.classList.replace('bi-list', 'bi-x-lg')
     }
 }
